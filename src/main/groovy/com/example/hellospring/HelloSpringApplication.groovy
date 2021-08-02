f73785ed68037f29edda5352944569b746f41648
viewadmin <gerardodaniel.diaz@viewnext.com>
2021-08-02 11:15:11 +0200

    Modificacion para hello-spring ,fixed path

--- src/main/groovy/com/example/hellospring/HelloSpringApplication.groovy
+++ src/main/groovy/com/example/hellospring/HelloSpringApplication.groovy
@@ -1,27 +1,27 @@
+package com.example.hellospring
 
-package com.example.demo;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+class HelloSpringApplication {
 
+    static void main(String[] args) {
+        SpringApplication.run(HelloSpringApplication, args)
+    }
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
-    @GetMapping(value = "/ride", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Iterable<ThemeParkRide> getRides() {
-        return themeParkRideRepository.findAll();
-    }
-    @GetMapping("/Gerardo")
-    public String hello(@RequestParam(value = "name", defaultValue = "Gerardo") String name) {
+    @GetMapping("/")
+    public String root(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
 
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 }
 
