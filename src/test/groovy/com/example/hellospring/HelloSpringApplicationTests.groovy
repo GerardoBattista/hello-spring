b31b80377bd08713eb523a88395b8a27330dd987
Gerardo Diaz <87467851+GerardoBattista@users.noreply.github.com>
2021-08-12 17:04:07 +0200

    Update HelloSpringApplicationTests.groovy
    error prompt fixed

--- src/test/groovy/com/example/hellospring/HelloSpringApplicationTests.groovy
+++ src/test/groovy/com/example/hellospring/HelloSpringApplicationTests.groovy
@@ -1,4 +1,4 @@
-ackage com.example.hellospring
+package com.example.hellospring
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
